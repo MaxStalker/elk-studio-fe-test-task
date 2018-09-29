f0510f5abbe499250d18a8e22145ce759b5c1a7e
max <max.daunarovich@gmail.com>
2018-09-30 01:21:22 +0300

    Define api urls in separate file

---
+++ src/helpers/api.js
@@ -0,0 +1,3 @@
+export const API_ROOT = 'https://papi-stage.contentmedia.eu/2.0'
+export const AUTHENTICATE_URL = `${API_ROOT}/auth/authenticate`
+export const ROUND_LIST_URL = `${API_ROOT}/roundhistory/rounds`
