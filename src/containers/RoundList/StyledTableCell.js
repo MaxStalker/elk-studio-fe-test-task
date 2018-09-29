cb8eedd9e4c476f6ea1f057377be98818075df94
max <max.daunarovich@gmail.com>
2018-09-30 01:25:00 +0300

    Create StyledTableCell component

---
+++ src/containers/RoundList/StyledTableCell.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { TableCell, withStyles } from '@material-ui/core'
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell)
+
+export default StyledTableCell
