af2261953602fc982f73cc7cca339007740f9feb
max <max.daunarovich@gmail.com>
2018-10-01 01:36:52 +0300

    Move AppBar render routine to standalone file

---
+++ src/containers/RoundList/AppBarRenderer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  withStyles,
+} from '@material-ui/core'
+import { Eject } from '@material-ui/icons'
+import { styles } from './styles'
+import classNames from 'classnames'
+
+const AppBarRenderer = ({ actions: { logout }, classes }) => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography
+          variant="title"
+          color="inherit"
+          className={classNames(classes.grow, classes.appBarTitle)}
+        >
+          Rounds List
+        </Typography>
+        <IconButton
+          title={'Logout'}
+          role={'button'}
+          aria-label={'logout-action'}
+          onClick={logout}
+          color="inherit"
+        >
+          <Eject />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default withStyles(styles)(AppBarRenderer)
