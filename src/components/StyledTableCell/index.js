3d3fdd8b18557e29cae39cb16017a86f827209a1
max <max.daunarovich@gmail.com>
2018-10-01 01:39:23 +0300

    Refactor TableCell to use theme colors

--- src/components/StyledTableCell/index.js
+++ src/components/StyledTableCell/index.js
@@ -3,12 +3,15 @@ import { TableCell, withStyles } from '@material-ui/core'
 
 const StyledTableCell = withStyles(theme => ({
   head: {
-    backgroundColor: theme.palette.common.black,
+    backgroundColor: theme.palette.secondary.main,
     color: theme.palette.common.white,
     fontWeight: 'bold',
+    padding: theme.spacing.unit * 2,
+    fontSize: 16,
   },
   body: {
     fontSize: 14,
+    padding: theme.spacing.unit * 2,
   },
 }))(TableCell)
 
