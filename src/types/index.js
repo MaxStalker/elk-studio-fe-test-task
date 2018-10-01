6a191d14444334c7f65ff58ce0acf3133985a4ff
max <max.daunarovich@gmail.com>
2018-10-02 02:46:16 +0300

    Setup some basic types

---
+++ src/types/index.js
@@ -0,0 +1,27 @@
+export type Action = { type: string, payload?: {}, error?: {} }
+export type Dispatch = (action: Action) => void
+
+type FetchRoundsParams = {
+  accountId: number,
+  dateFrom: string,
+  operatorId: number,
+}
+
+type Round = {
+  status: string,
+  gameName: string,
+  id: number,
+  operatorId: number,
+  gameId: number,
+  accountId: number,
+  purchaseMode: string,
+  clientMode: string,
+  currency: string,
+  totalBet: number,
+  totalWin: number,
+  feature: boolean,
+  competitionAffected: boolean,
+  formated?: {
+    created: Date,
+  },
+}
