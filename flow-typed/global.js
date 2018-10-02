76d88af18e87ee6ad9ab7c83b2368bca6ecf4667
max <max.daunarovich@gmail.com>
2018-10-03 01:17:37 +0300

    Create global definition file

---
+++ flow-typed/global.js
@@ -0,0 +1,44 @@
+//@flow
+declare type Action = { type: string, payload?: { body?: {} }, error?: {} }
+declare type Dispatch = (action: Action) => void
+
+declare type ThunkActionCreator = () => any
+declare type ThunkAction = ({}) => any
+
+declare type FetchRoundsParams = {
+  accountId: number,
+  dateFrom: string,
+  operatorId: number,
+}
+
+declare type Round = {
+  status: string,
+  gameName: string,
+  id: number,
+  operatorId: number,
+  gameId: number,
+  created: string,
+  accountId: number,
+  purchaseMode: string,
+  clientMode: string,
+  create: string,
+  currency: string,
+  totalBet: number,
+  totalWin: number,
+  feature: boolean,
+  competitionAffected: boolean,
+  formated: {
+    created: Date,
+  },
+}
+
+declare type Theme = {
+  spacing?: {
+    unit?: number,
+  },
+  palette?: {
+    background?: {
+      default: string,
+    },
+  },
+}
